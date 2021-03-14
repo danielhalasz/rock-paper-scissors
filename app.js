614391fcaf17fac71a97ca47c40e1ce3ddc2f6fa
Daniel Halasz <halaszdaniel@gmail.com>
2021-03-14 12:34:48 +0100

    1 round game

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 
-function computerPlay(){
+let playerPlay = prompt('Rock, paper or scissors?').toLowerCase();
+
+let computerPlay = myFunction()
+
+function myFunction() {
     let randomValue = Math.floor((Math.random() * 3) + 1); 
     if (randomValue === 1) {
         return 'rock';
@@ -8,6 +12,33 @@ function computerPlay(){
     } else {
         return 'scissors';
     }
+}    
+
+function playRound (computerPlay, playerPlay) {
+    if (computerPlay === 'rock' && playerPlay === 'scissors')
+    alert("You Lose! Rock beats scissors");  
+    if (computerPlay === 'rock' && playerPlay === 'rock')
+    alert("Draw");
+    if (computerPlay === 'rock' && playerPlay === 'paper')
+    alert("You Win! Paper beats rock");
+    
+    if (computerPlay === 'paper' && playerPlay === 'scissors')
+    alert("You Win! Scissors beat paper");  
+    if (computerPlay === 'paper' && playerPlay === 'rock')
+    alert("You Lose! Paper beats rock");
+    if (computerPlay === 'paper' && playerPlay === 'paper')
+    alert("Draw");
+
+    if (computerPlay === 'scissors' && playerPlay === 'scissors')
+    alert("Draw");  
+    if (computerPlay === 'scissors' && playerPlay === 'rock')
+    alert("You Win! Rock beats scissors");
+    if (computerPlay === 'scissors' && playerPlay === 'paper')
+    alert("You Lose! Scissors beat paper");
 }
 
-console.log(computerPlay());
+console.log(computerPlay);
+console.log(playerPlay);
+console.log(playRound(computerPlay, playerPlay));
+
+
